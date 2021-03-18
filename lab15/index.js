2f59bd515a5029e7386dee28fde17f84d89630f1
dvysochkin <v2017-78@yandex.ru>
2021-03-18 08:03:42 +0000

    lab15

---
+++ lab15/index.js
@@ -0,0 +1,2 @@
+var generateImage = require('random-image-creator')
+console.log(generateImage(280,400));
---
+++ main.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var generateImage = require('random-image-creator')
+ 
+http.createServer(function(request, response){
+     
+     
+    console.log("Url: " + request.url);
+    console.log("Тип запроса: " + request.method);
+    console.log("User-Agent: " + request.headers["user-agent"]);
+    console.log("Все заголовки");
+    console.log(request.headers);
+    response.writeHead(200, {
+    'Content-Type': 'text/html'
+  })
+    const lad = generateImage(280,400);
+     
+    response.end("<img src='"+lad+"'>");
+}).listen(3000);
